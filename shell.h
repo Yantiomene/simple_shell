02375338c52f8a0e055ca53cfa0a7ff8ee269e40
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-17 16:26:07 -0700

    [Update] adding new function prototypes

--- shell.h
+++ shell.h
@@ -7,12 +7,14 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 char *readline(int *eof);
 int _strlen(char *s);
 char *_strdup(const char *str);
 char **split_line(char *line);
-void exec_cmd(char **args, char **env);
+char *get_cmd_path(char **av, char *cmd);
+void exec_cmd(char **av, char **args, char **env);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strcpy(char *dest, char *src);
 
