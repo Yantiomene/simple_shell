c2cd72a5b1845acbc65640fcbb93f3d0eca01df8
Uchenna Oko <Uche4newton1981@gmail.com>
2023-04-17 16:16:34 +0000

    Updated-Adding a function that reads a line from the stdin.

--- readline.c
+++ readline.c
@@ -1,43 +1,17 @@
 #include "shell.h"
 
-
 /**
- * _getline -  program that displays a prompt, wait for the user to enter a command,
- * prints it on the next line
- * @line: pointer to the buffer
- * @n: size of the character
+ * readline - read line from the stdin
+ * @eof: pointer to an integer
  *
- * Return: 0 if successful.
+ * Return: the line just read                                    
  */
-ssize_t _getline(char **line, size_t *n, FILE *stream)
+char *readline(int *eof)
 {
-	char *buf = NULL;
-	size_t len = 0;
-	ssize_t read;
+	char *line = NULL;
+	size_t n = 0;
 
-	buf = malloc(sizeof(char) * len);
+	*eof = getline(&line, &n, stdin);
 
-	if (buf == NULL)
-	{
-		printf("Memory allocation failed\n");
-		return (0);
-	}
-
-	printf("simple_shell$ ");
-	read = getline(&buf, &len, stdin); /*Read input from user*/
-
-	if (read == -1) /*Check for end-of-file or error*/
-	{
-		printf("Error reading input.\n");
-		return (-1);
-	}
-
-	else
-	{
-		printf("Command: %s", buf); /*Print the entered command*/
-	}
-
-	free(buf); /*Free the memory allocated by getline*/
-
-	return (0);
+	return (line);
 }
