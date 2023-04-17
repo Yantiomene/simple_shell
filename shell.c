4c8c4ec561a365adf2c902dc0c1f7cf02c6d93cc
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-17 08:28:46 -0700

    [Fix]: Handling the EOF condition

--- shell.c
+++ shell.c
@@ -13,14 +13,22 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 {
 	char *line = NULL, *prompt = "YOsh$ ";
 	char **args;
+	int state = 1, eof;
 
 	(void)av;
 	do {
 		write(STDOUT_FILENO, prompt, 6);
-		line = readline(&line);
-		args = split_line(line);
-		exec_cmd(args, env);
-	} while (1);
+		line = readline(&eof);
+		if (eof == -1) /* End of file*/
+		{
+			state = 0;
+		}
+		else
+		{
+			args = split_line(line);
+			exec_cmd(args, env);
+		}
+	} while (state);
 	free(line);
 	free(args);
 	return (0);
