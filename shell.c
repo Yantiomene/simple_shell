bec25f1ce5ce87d6598be13e45b9c01b70f2c336
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-17 10:54:26 -0700

    [Fix] correcting the loop to match the new prototype of readline

--- shell.c
+++ shell.c
@@ -11,24 +11,25 @@
 
 int main(int ac __attribute__((unused)), char **av, char **env)
 {
-	char *line = NULL, *prompt = "YOsh$ ";
+	char *line = NULL, *prompt = "(YOsh)$ ";
 	char **args;
 	int state = 1, eof;
 
 	(void)av;
-	do {
-		write(STDOUT_FILENO, prompt, 6);
+	while (state)
+	{
+		write(STDIN_FILENO, prompt, 8);
 		line = readline(&eof);
-		if (eof == -1) /* End of file*/
-		{
-			state = 0;
-		}
-		else
+		if (eof != -1) /* End of file*/
 		{
 			args = split_line(line);
 			exec_cmd(args, env);
 		}
-	} while (state);
+		else
+		{
+			state = 0;
+		}
+	}
 	free(line);
 	free(args);
 	return (0);
