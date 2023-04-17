059b25310610d532acf9c23ea21ca7c5336b84dd
Uchenna Oko <Uche4newton1981@gmail.com>
2023-04-17 16:24:03 +0000

    Updated-Adding a header file.

--- shell.h
+++ shell.h
@@ -4,14 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-ssize_t _getline(char **line, size_t *n, FILE *stream);
+char *readline(int *eof);
 int _strlen(char *s);
 char *_strdup(const char *str);
-int main(int ac __attribute__((unused)), char **av, char **env);
 char **split_line(char **line);
 void exec_cmd(char **args, char **env);
 int _strncmp(const char *s1, const char *s2, size_t n);
