03dbf98284b8f1ec80c48066aa8f4fdc88a00c58
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-17 10:58:17 -0700

    [Fix] Fixing return value and variable declarations

--- split.c
+++ split.c
@@ -1,4 +1,4 @@
-#include "shell.c"
+#include "shell.h"
 
 /**
  * split_line - parse the input line
@@ -7,11 +7,11 @@
  * Return: array of strings
  */
 
-char **split_line(char **line)
+char **split_line(char *line)
 {
 	char *line_cpy, *token, *sep = " \t\n\r\a";
 	char **args;
-	int num_tokens, i = 0;
+	int num_token = 0, i;
 
 	line_cpy = _strdup(line);
 	token = strtok(line_cpy, sep);
@@ -22,19 +22,20 @@ char **split_line(char **line)
 		token = strtok(NULL, sep);
 	}
 
-	args = malloc(sizeof(char *) * num_token);
+	args = malloc(sizeof(char *) * (num_token +1));
 	if (args == NULL)
 	{
-		write(STDERR_FILENO, "Yosh: Allocation error\n", 23);
+		write(STDERR_FILENO, ": Allocation error\n", 19);
 		return (NULL);
 	}
 
 	token = strtok(line, sep);
-	for (; i < num_token; i++)
+	for (i = 0; i < num_token; i++)
 	{
 		args[i] = token;
 		token = strtok(NULL, sep);
 	}
+	args[i] = NULL;
 	free(line_cpy);
 	return (args);
 }
