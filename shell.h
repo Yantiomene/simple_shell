dd63c3ac3076a197cb57b3790ed6cd2c873fbec5
Uchenna Oko <Uche4newton1981@gmail.com>
2023-04-18 20:16:57 +0000

    Updated-header file

--- shell.h
+++ shell.h
@@ -15,6 +15,17 @@ char **split_line(char *line);
 void exec_cmd(char **args, char **env);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strcpy(char *dest, char *src);
+int exec_bin(char **args);
+char *_strcat(char *s1, char *s2);
+char _getenv(char *path);
+char *get_cmd_path(char *cmd);
 
+typedef struct builtin
+{
+	char *name;
+	void(*func)(char **args _attribute_((unused)));
+}bin_t;
+
+void bin_exit(char **args _attribute_((unused)));
 
 #endif
