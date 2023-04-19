1f395495cd3a4344be64c45329db8fed080ffa04
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-19 16:36:11 -0700

    "[Fix] correcting the betty style"

--- _getenv.c
+++ _getenv.c
@@ -6,15 +6,14 @@
  *
  * Return: pointer to variable or NULL, if variable is not found.
  */
-char *_getenv(const char *path)
+char *_getenv(char *path)
 {
 	size_t nam_len = _strlen(path), i;
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		if (_strncmp(path, environ[i], nam_len) == 0 && environ[i][nam_len] == '=')
-
-			return (environ[i] + nam_len + 1);
+	if (_strncmp(path, environ[i], nam_len) == 0 && environ[i][nam_len] == '=')
+		return (environ[i] + nam_len + 1);
 	}
 
 	return (NULL);
