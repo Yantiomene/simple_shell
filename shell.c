88e6a6f9b8add33037b3dad98de69790fb7bf80e
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-27 10:59:32 -0700

    [Fix] Fixing leak memory

--- shell.c
+++ shell.c
@@ -35,10 +35,9 @@ int main(int ac __attribute__((unused)), char **av)
 		{
 			state = 0;
 		}
+		free(data.line);
 
 	}
-	if (data.line)
-		free(data.line);
 	free_data(&data);
 	return (data.status);
 }
